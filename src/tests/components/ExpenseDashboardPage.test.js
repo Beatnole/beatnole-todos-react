06cef2b7f74c0568173c53d238f1589234e8ef84
patrick@beatnole.com <patrick@beatnole.com>
2018-11-27 07:45:59 -0500

    Init Commit v2

--- src/tests/components/ExpenseDashboardPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ExpenseDashboardPage from '../../components/ExpenseDashboardPage'
-import { shallow } from 'enzyme'
-
-
-test('ExpenseDashboard Display with 3 expenses', () => {
-    const wrapper = shallow(<ExpenseDashboardPage />)
-    expect(wrapper).toMatchSnapshot()
-} )
--- src/tests/components/LoadingPage.test.js
+++
@@ -1,8 +0,0 @@
-import { shallow } from 'enzyme'
-import React from 'react'
-import LoadingPage from '../../components/LoadingPage'
-
-test('loading page snapshot test', () => {
-    const wrapper = shallow(<LoadingPage />)
-    expect(wrapper).toMatchSnapshot()
-})
